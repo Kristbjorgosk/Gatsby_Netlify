e4362131785644f32ebee53e17889bc63466e4b6
kristbjörg <kristbjorg_@hotmail.com>
2021-02-27 18:15:30 +0000

    potfolio props + layout

--- src/pages/blogIndex.js
+++ src/pages/blogIndex.js
@@ -26,7 +26,7 @@ const BlogIndex = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="All posts" />
       <Bio />
-      <p>It WORKS</p>
+     
       <ol style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
--- src/templates/projects.js
+++ src/templates/projects.js
@@ -1,28 +1,23 @@
 import React from "react"
 import { Card } from "react-bootstrap"
 
-function projects(props) {
+const Projects = ({ data, location }) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroupItem>Cras justo odio</ListGroupItem>
-        <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-        <ListGroupItem>Vestibulum at eros</ListGroupItem>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
+    <Layout location={location} title={siteTitle}>
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
+        <Card.Body>
+          <Card.Title> {post.frontmatter.title} </Card.Title>
+          <Card.Text>{post.frontmatter.description}</Card.Text>
+          <Card.Link href="#">{post.frontmatter.liveDemo}</Card.Link>
+          <Card.Link href="#">{post.frontmatter.github}</Card.Link>
+        </Card.Body>
+      </Card>
+    </Layout>
   )
 }
 
-export default projects
+export default Projects
